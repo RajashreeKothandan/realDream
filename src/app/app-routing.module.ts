875913c952d85f8f7176db262540976df316d98e
RajashreeKothandan <ranjithakothandan@gmail.com>
2020-05-31 21:55:37 +0530

    Routing for home page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import{HomeComponent} from'../app/home/home.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents=[HomeComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule,routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home/home.component';
+import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { ServiceComponent } from './service/service.component';
@@ -20,7 +20,7 @@ import { GalleryComponent } from './gallery/gallery.component';
     ServiceComponent,
     BlogComponent,
     EventComponent,
-    GalleryComponent
+    GalleryComponent,routingComponents
   ],
   imports: [
     BrowserModule,
