ea215c220933280eff28d05f9f0a9c8c69d5573d
RajashreeKothandan <ranjithakothandan@gmail.com>
2020-06-01 21:37:53 +0530

    Routing and other petty changes done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import{HomeComponent} from'../app/home/home.component'
+import{AboutComponent} from'../app/about/about.component'
+import{ServiceComponent} from'../app/service/service.component'
+import{EventComponent} from'../app/event/event.component'
+import{ContactComponent} from'../app/contact/contact.component'
+
+
 
 
 const routes: Routes = [
-  {path:'home',component:HomeComponent}
+  {path:'',component:HomeComponent},
+  {path:'about',component:AboutComponent},
+  {path:'services',component:ServiceComponent},
+  {path:'activities',component:EventComponent},
+  {path:'donate',component:AboutComponent},
+  {path:'contact',component:ContactComponent},
+ { path: '**',   redirectTo: ''}
 ];
 
 @NgModule({
@@ -12,4 +24,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents=[HomeComponent];
+export const routingComponents=[HomeComponent,AboutComponent];
